681d2e3042140b3fefa4b5bdf96e6d64cf35d7fd
blakestack <66808440+blakestack@users.noreply.github.com>
2026-01-13 13:23:36 -0500

    Add 'api' folder and set up environment varialbes

---
+++ whosontop/src/app/api/teams/route.ts
@@ -0,0 +1,5 @@
+export async function GET() {
+    // Logic to fetch data from a database or external service
+    const data = {};
+    return Response.json(data); // Use the Web Standard Response API
+  }
